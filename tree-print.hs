f65b27915c9344a105a5c0f267e3e9e85b88d213
Christoph Rueff <christoph.rueff@gmail.com>
2020-05-19 22:36:01 +0200

    modify for ints

--- tree-print.hs
+++ tree-print.hs
@@ -1,12 +1,12 @@
 data BinaryTree a = Nil | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
 t = Node 0 (Node 1 Nil Nil) (Node 2 Nil Nil)
-t2 = Node 'a' (Node 'b' Nil Nil) (Node 'c' Nil Nil)
-t3 = Node 'a' (Node 'b' (Node 'd' Nil Nil) (Node 'e' Nil Nil)) (Node 'c' (Node 'f' Nil Nil) (Node 'g' Nil Nil))
-t4 = Node 'a' (Node 'b' Nil (Node 'e' Nil Nil)) (Node 'c' (Node 'f' Nil Nil) (Node 'g' Nil Nil))
-t5 = Node 'a' (Node 'b' (Node 'd' Nil Nil) Nil) (Node 'c' (Node 'f' Nil Nil) Nil)
-t6 = Node 'a' (Node 'b' (Node 'c' (Node 'd' Nil Nil) Nil) Nil) Nil
-udc = Node 'a' Nil (Node 'b' Nil (Node 'c' Nil (Node 'd' Nil Nil)))
+t3 = Node 1 (Node 2 (Node 4 Nil Nil) (Node 5 Nil Nil)) (Node 3 (Node 6 Nil Nil) (Node 7 Nil Nil))
+t4 = Node 1 (Node 2 Nil (Node 5 Nil Nil)) (Node 3 (Node 6 Nil Nil) (Node 7 Nil Nil))
+-- t5 = Node 'a' (Node 'b' (Node 'd' Nil Nil) Nil) (Node 'c' (Node 'f' Nil Nil) Nil)
+-- t6 = Node 'a' (Node 'b' (Node 'c' (Node 'd' Nil Nil) Nil) Nil) Nil
+-- t7 = Node 'a' (Node 'b' (Node 'd' (Node '1' Nil Nil) (Node '2' Nil Nil)) (Node 'e' (Node '3' Nil Nil) (Node '4' Nil Nil))) (Node 'c' (Node 'f' (Node '5' Nil Nil) (Node '6' Nil Nil)) (Node 'g' (Node '7' Nil Nil) (Node '8' Nil Nil)))
+-- udc = Node 'a' Nil (Node 'b' Nil (Node 'c' Nil (Node 'd' Nil Nil)))
 
 spaces 0 = ""
 spaces n = ' ' : spaces (n - 1)
@@ -14,7 +14,11 @@ spaces n = ' ' : spaces (n - 1)
 repeat' s 0 = ""
 repeat' s n = s ++ (repeat' s (n - 1))
 
-interSpaces h l = 2*(h - l) + 1
+interSpaces h l = 
+  if l == h then
+    1
+  else 
+    2 * (interSpaces h (l + 1)) + 1
 
 height Nil = 0
 height (Node _ left right) =
@@ -26,9 +30,9 @@ height (Node _ left right) =
 
 levelToString height level tree = helper height level level tree
   where
-    helper h l 1 Nil = '-' : spaces (interSpaces h l)
-    helper h l i Nil = repeat' ('-' : spaces (interSpaces h l)) (2**(l - i))
-    helper h l 1 (Node a left right) = a : spaces (interSpaces h l)
+    helper h l 1 Nil = '.' : spaces (interSpaces h l)
+    helper h l i Nil = repeat' ('.' : spaces (interSpaces h l)) (2**(l - i))
+    helper h l 1 (Node a left right) = (show a) ++ spaces (interSpaces h l)
     helper h l i (Node a left right) =
       helper h l (i - 1) left ++ helper h l (i - 1) right
 
