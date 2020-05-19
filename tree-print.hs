d80289dd5cfd8aa2233f1c0222919b85e22872dd
Christoph Rueff <christoph.rueff@gmail.com>
2020-05-19 21:44:14 +0200

    Fix holes

--- tree-print.hs
+++ tree-print.hs
@@ -1,29 +1,50 @@
-data BinaryTree a = Nil | Leaf a | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
+data BinaryTree a = Nil | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
-t = Node 0 (Leaf 1) (Leaf 2)
-t2 = Node 'a' (Leaf 'b') (Leaf 'c')
-t3 = Node 'a' (Node 'b' (Leaf 'd') (Leaf 'e')) (Node 'c' (Leaf 'f') (Leaf 'g'))
-t4 = Node 'a' (Node 'b' Nil (Leaf 'e')) (Node 'c' (Leaf 'f') (Leaf 'g'))
-t5 = Node 'a' (Node 'b' Nil (Leaf 'e')) (Node 'c' (Node 'z' (Leaf 'x') Nil) (Leaf 'g'))
+t = Node 0 (Node 1 Nil Nil) (Node 2 Nil Nil)
+t2 = Node 'a' (Node 'b' Nil Nil) (Node 'c' Nil Nil)
+t3 = Node 'a' (Node 'b' (Node 'd' Nil Nil) (Node 'e' Nil Nil)) (Node 'c' (Node 'f' Nil Nil) (Node 'g' Nil Nil))
+t4 = Node 'a' (Node 'b' Nil (Node 'e' Nil Nil)) (Node 'c' (Node 'f' Nil Nil) (Node 'g' Nil Nil))
+t5 = Node 'a' (Node 'b' (Node 'd' Nil Nil) Nil) (Node 'c' (Node 'f' Nil Nil) Nil)
+t6 = Node 'a' (Node 'b' (Node 'c' (Node 'd' Nil Nil) Nil) Nil) Nil
+udc = Node 'a' Nil (Node 'b' Nil (Node 'c' Nil (Node 'd' Nil Nil)))
 
-spaces 0 a = show a ++ "\n"
-spaces n a = ' ' : ' ' : spaces (n - 1) a
+spaces 0 = ""
+spaces n = ' ' : spaces (n - 1)
 
-toStringH Nil = "Empty Tree\n"
-toStringH bt = ts 0 bt
-  where
-    ts l (Leaf a) = spaces l a
-    ts l Nil = ""
-    ts l (Node a left right) = (ts (l + 1) right) ++ (spaces l a) ++ (ts (l + 1) left)
+repeat' s 0 = ""
+repeat' s n = s ++ (repeat' s (n - 1))
 
-printTreeHorizontal t = do
-  putStr (toStringH t)
+interSpaces h l = 2*(h - l) + 1
 
 height Nil = 0
-height (Leaf _) = 0
 height (Node _ left right) =
   let
     hl = height left
     hr = height right
   in
     (if hl > hr then hl else hr) + 1
+
+levelToString height level tree = helper height level level tree
+  where
+    helper h l 1 Nil = '-' : spaces (interSpaces h l)
+    helper h l i Nil = repeat' ('-' : spaces (interSpaces h l)) (2**(l - i))
+    helper h l 1 (Node a left right) = a : spaces (interSpaces h l)
+    helper h l i (Node a left right) =
+      helper h l (i - 1) left ++ helper h l (i - 1) right
+
+printLevels height tree = do levelsPrintLoop 1 height tree
+  where
+    levelsPrintLoop i height tree = do
+      putStr (spaces (2**(height - i) - 1))
+      putStr (levelToString height i tree)
+      if i == height then do
+        putStr "\n"
+      else do
+        putStr "\n"
+        levelsPrintLoop (succ i) height tree
+
+printTree tree = do
+  let h = height tree
+  printLevels h tree
+
+
